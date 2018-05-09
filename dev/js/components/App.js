03941616d0bfd4dffe564f5b5a76662282657a99
JohnsonLu3 <JohnsonLu94@gmail.com>
2018-05-09 11:48:56 -0400

    Redoing the site with Redux React.js

---
+++ dev/js/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../containers/Header';
+import Footer from '../containers/Footer';
+import Board from '../containers/Board';
+import Note from '../containers/note';
+
+require('../../scss/style.scss');
+
+const App = () => (
+    <div>
+    </div>
+);
+
+export default App;
