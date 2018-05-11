d84eff3883712f4ef18ecab0a98cf87fb40f5aba
JohnsonLu3 <JohnsonLu94@gmail.com>
2018-05-11 17:13:11 -0400

    added Contact Component

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -4,6 +4,7 @@ import Footer from '../containers/Footer';
 import Bio from '../containers/Bio';
 import Skills from '../containers/Skills';
 import ShowCase from '../containers/ShowCase';
+import Contact from '../containers/Contact';
 
 require('../../scss/style.scss');
 
@@ -16,6 +17,11 @@ const App = () => (
         <br />
         <ShowCase />
         <Footer />
+
+        <Contact />
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     </div>
 );
 
---
+++ dev/js/containers/Contact.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+
+class Contact extends Component{
+
+    render(){
+        let Bio = this.props.Bio;
+        return(
+            <div className="modal fade" id="modalContact" role="dialog">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title">Contact Info</h5>
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">×</span> </button>
+                    </div>
+                    <div className="modal-body">
+                        <div className="row">
+                        <div className="col-md-12">
+                            <div className="row">
+                            <div className="col-md-12">
+                                <p className="lead">{Bio.Name}</p>
+                            </div>
+                            </div>
+                            <div className="row">
+                            <div className="col-md-12">
+                                <p className="">Email: {Bio.Email}}</p>
+                            </div>
+                            </div>
+                            <div className="row">
+                            <div className="col-md-6 d-flex justify-content-center">
+                                <a className="btn btn-primary" href={Bio.Github}>GitHub</a>
+                            </div>
+                            <div className="col-md-6 d-flex justify-content-center">
+                                <a className="btn btn-primary" href={Bio.LinkedIn}>LinkedIn</a>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        Bio: state.Bio
+    };
+}
+
+// Actions
+function matchDispatchToProps(dispatch){
+    return bindActionCreators(
+        {
+        },
+         dispatch);
+}
+
+export default connect(mapStateToProps,matchDispatchToProps)(Contact);
