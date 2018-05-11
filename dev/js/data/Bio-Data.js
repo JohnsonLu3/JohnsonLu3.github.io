75bcab05a598e75539b96ee66c889947a9fbea19
JohnsonLu3 <JohnsonLu94@gmail.com>
2018-05-11 13:01:57 -0400

    added data files and imgs

---
+++ dev/js/data/Bio-Data.js
@@ -0,0 +1,7 @@
+const bio = {
+    Name : "Johnson Lu",
+    Desc : "Full stack developer, graduated at Suny Stony Brook with a Bachelor of Science in Fall 2017. My interest includes system and web security, as well as machine Learning. I'm currently trying to create a snake game that plays itself using a shallow neural network. In my free time I enjoy making mock websites, prototype programs and games.",
+    Email : "JohnsonLu404@gmail.com",
+    Github: "https://github.com/JohnsonLu3",
+    LinkedIn: "https://www.linkedin.com/in/johlu1/"
+}
---
+++ dev/js/data/Skill-Data.js
@@ -0,0 +1,83 @@
+const skills = [
+
+    {
+        id: "HTML",
+        ico:  "devicon-html5-plain"
+    },
+    
+    {
+        id: "CSS",
+        ico: "devicon-css3-plain"
+    },
+    
+    {
+        id: "JS",
+        ico: "devicon-javascript-plain"
+    },
+
+    {
+        id: "JQUERY",
+        ico:  "devicon-jquery-plain"
+    },
+
+    {
+        id: "REACT",
+        ico:  "devicon-react-original"
+    },
+    
+    {
+        id: "JAVA",
+        ico:  "devicon-java-plain"
+    },
+    
+    {
+        id: "PYTHON",
+        ico:  "devicon-python-plain"
+    },
+    
+    {
+        id: "DJANGO",
+        ico: "devicon-django-plain"
+    },
+    
+    {
+        id: "MYSQL",
+        ico:  "devicon-mysql-plain-wordmark"
+    },
+
+    {
+        id: "GIT",
+        ico:  "devicon-git-plain"
+    },
+
+    {
+        id: "GRADLE",
+        ico:  "devicon-gradle-plain"
+    },
+
+    {
+        id: "LINUX",
+        ico:  "devicon-linux-plain"
+    },
+
+
+    {
+        id: "FOUNDATION",
+        ico:  "devicon-foundation-plain-wordmark"
+    },
+
+    {
+        id: "BOOTSTRAP",
+        ico: "devicon-bootstrap-plain"
+    },
+
+    {
+        id: "AWS",
+        ico: "devicon-amazonwebservices-plain-wordmark"
+    },
+
+    {
+        id: "ARDUNIO",
+        ico: "fas fa-microchip"
+    }
+]
