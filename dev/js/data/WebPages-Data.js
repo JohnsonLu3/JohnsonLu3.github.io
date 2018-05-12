b4e9a18a575b67d7533cb7825b4ad5722d4b2769
JohnsonLu3 <JohnsonLu94@gmail.com>
2018-05-12 14:45:07 -0400

    added data to webpage-data

--- dev/js/data/WebPages-Data.js
+++ dev/js/data/WebPages-Data.js
@@ -1,4 +1,27 @@
 const Webpages =[
-    {}
+    {
+        Id: "NOTESPACE",
+        Name: "NoteSpace",
+        Link: "https://johnsonlu3.github.io/NoteSpace/src/index.html",
+        Img: "",
+    },
+    {
+        Id: "YTSPOILERFREE",
+        Name: "YT Spoiler Free",
+        Link: "http://www.ytspoilerfree.com/",
+        Img: "",
+    },
+    {
+        Id: "GALLERYRICH",
+        Name: "Gallery Rich",
+        Link: "",
+        Img: "",
+    },
+    {
+        Id: "BLOGTEMP",
+        Name: "Blog Template",
+        Link: "",
+        Img: "",
+    }
 ]
 export default Webpages;
